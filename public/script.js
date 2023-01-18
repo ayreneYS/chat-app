367e32c6034f83eff58d0a7c9f28cecee512ced5
Ayrene <112762086+ayreneYS@users.noreply.github.com>
2023-01-18 17:35:06 +0800

    Updated script.js

--- public/script.js
+++ public/script.js
@@ -1,80 +1,79 @@
-(function () {
+(() => {
   const app = document.querySelector('.app');
   const socket = io();
-
+  const joinUserButton = app.querySelector('.login-page #join-user');
+  const sendMessageButton = app.querySelector('.chat-page #send-message');
+  const exitChatButton = app.querySelector('.chat-page #exit-chat');
+  const usernameInput = app.querySelector('.login-page #username');
+  const messageInput = app.querySelector('.chat-page #message-input');
+  const messageContainer = app.querySelector('.chat-page .messages');
   let inputName;
 
-  app
-    .querySelector('.login-page #join-user')
-    .addEventListener('click', function (e) {
-      e.preventDefault();
-      let username = app.querySelector('.login-page #username').value;
-      if (username.length == 0) {
-        return;
-      }
-      socket.emit('newuser', username);
-      inputName = username;
-      app.querySelector('.login-page').classList.remove('active');
-      app.querySelector('.chat-page').classList.add('active');
-    });
+  joinUserButton.addEventListener('click', handleJoinUserClick);
+  sendMessageButton.addEventListener('click', handleSendMessageClick);
+  exitChatButton.addEventListener('click', handleExitChatClick);
 
-  app
-    .querySelector('.chat-page #send-message')
-    .addEventListener('click', function () {
-      let message = app.querySelector('.chat-page #message-input').value;
-      if (message.length == 0) {
-        return;
-      }
-      renderMessage('my', {
-        username: inputName,
-        text: message,
-      });
-      socket.emit('chat', {
-        username: inputName,
-        text: message,
-      });
-      app.querySelector('.chat-page #message-input').value = '';
-    });
+  socket.on('update', renderUpdate);
+  socket.on('chat', renderChat);
 
-  app
-    .querySelector('.chat-page #exit-chat')
-    .addEventListener('click', function () {
-      socket.emit('exituser', inputName);
-      window.location.href = window.location.href;
-    });
+  function handleJoinUserClick(e) {
+    e.preventDefault();
+    const username = usernameInput.value;
+    if (username.length === 0) return;
+    socket.emit('newuser', username);
+    inputName = username;
+    app.querySelector('.login-page').classList.remove('active');
+    app.querySelector('.chat-page').classList.add('active');
+  }
 
-  socket.on('update', function (update) {
+  function handleSendMessageClick() {
+    const message = messageInput.value;
+    if (message.length === 0) return;
+    renderMessage('my', {
+      username: inputName,
+      text: message,
+    });
+    socket.emit('chat', {
+      username: inputName,
+      text: message,
+    });
+    messageInput.value = '';
+  }
+
+  function handleExitChatClick() {
+    socket.emit('exituser', inputName);
+    window.location.href = window.location.href;
+  }
+
+  function renderUpdate(update) {
     renderMessage('update', update);
-  });
+  }
 
-  socket.on('chat', function (message) {
+  function renderChat(message) {
     renderMessage('other', message);
-  });
+  }
 
   function renderMessage(type, message) {
-    let messageContainer = app.querySelector('.chat-page .messages');
-    if (type == 'my') {
-      let el = document.createElement('div');
+    if (type === 'my') {
+      const el = document.createElement('div');
       el.setAttribute('class', 'message my-message');
       el.innerHTML = `
-              <div>
-                  <div class='name'>You</div>
-                  <div class='text'>${message.text}</div>
-              </div>
-              `;
+        <div>
+            <div class='name'>You</div>
+            <div class='text'>${message.text}</div>
+            </div>`;
       messageContainer.appendChild(el);
-    } else if (type == 'other') {
-      let el = document.createElement('div');
+    } else if (type === 'other') {
+      const el = document.createElement('div');
       el.setAttribute('class', 'message other-message');
-      el.innerHTML = `
-              <div>
-                  <div class='name'>${message.username}</div>
-                  <div class='text'>${message.text}</div>
-              </div>
-              `;
+      el.innerHTML = `<div>
+            <div class='name'>${message.username}</div>
+            <div class='text'>${message.text}</div>
+            </div>
+            `;
       messageContainer.appendChild(el);
-    } else if (type == 'update') {
-      let el = document.createElement('div');
+    } else if (type === 'update') {
+      const el = document.createElement('div');
       el.setAttribute('class', 'update');
       el.innerText = message;
       messageContainer.appendChild(el);
